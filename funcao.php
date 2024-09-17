32fd4303c99777c412186ee81815ee8025729a9b
Macario1310 <159207820+Macas1310@users.noreply.github.com>
2024-09-17 20:12:21 -0300

    att

---
+++ funcao.php
@@ -0,0 +1,27 @@
+<?php 
+
+// $numero1 = 13;
+
+// $numero2 = 14;
+
+// $resultado = $numero1 + $numero2;
+// echo "sua soma é: $resultado";
+
+// implementacao de funcao de soma
+
+    function soma($n1,$n2){ 
+        $resultado = $n1+$n2;
+        echo $resultado;
+}
+
+soma(10,5);
+
+
+
+
+
+
+
+
+
+?>
