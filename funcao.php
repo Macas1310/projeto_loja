af940c2fac10f6a63d434b81da324c1dfe5878b8
Macario1310 <159207820+Macas1310@users.noreply.github.com>
2024-09-17 21:01:12 -0300

    att

--- funcao.php
+++ funcao.php
@@ -11,17 +11,32 @@
 
     function soma($n1,$n2){ 
         $resultado = $n1+$n2;
-        echo $resultado;
+        echo "O resultado é: $resultado";
+        echo"<br>";
 }
-
+echo "<br>";
 soma(10,5);
+echo "<br>";
+soma(50,40);        
 
 
 
+function subtracao($n1,$n2){    
+    $resultado = $n1-$n2;
+    echo "O resultado é: $resultado";
+}
+echo"<br>";
+subtracao(10,2);
+echo"<br>";
 
+echo"<br>";
 
-
-
+function divisao($n1,$n2){    
+    $resultado = $n1/$n2;
+    echo "O resultado é: $resultado";
+}
+divisao(50,10);
+echo"<br>";
 
 
 ?>
